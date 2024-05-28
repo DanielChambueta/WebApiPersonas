0745df4a91bfca25e8bdaea423f302b4f0b6eb70
Daniel Chambueta <83682738+DanielChambueta@users.noreply.github.com>
2024-05-28 17:57:23 -0500

    Agregar archivos de proyecto.

---
+++ WebApiPersonas/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiPersonas.Models;
+
+namespace WebApiPersonas.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}
